1c1a7d077fe62564d101e27c88230a6d557e478a
Dan Benitah <bdanuw@gmail.com>
2024-09-06 16:36:15 +0100

    add assets in CLI - not confirmed if MQTT simulations works through that method yet

--- get-started/3-quickstart-add-assets.sh
+++ get-started/3-quickstart-add-assets.sh
@@ -4,18 +4,43 @@ if [ -z "$SUBSCRIPTION_ID" ]; then
     exit 1
 fi
 
+# Define the variable
+CONNECTOR_NAME="opc-ua-connector-0"
+CONNECTOR_URL="opc.tcp://opcplc-000000:50000"
+NUM_THERMOSTATS=3
+
+# Example usage in the script
+echo "Using connector: $CONNECTOR_NAME at $CONNECTOR_URL and adding $NUM_THERMOSTATS thermostats"
+
 echo ""
 echo "#### Set up OPC UA ####"
 
 echo "Create an asset endpoint"
-az iot ops asset endpoint create --name opc-ua-connector-0 --target-address opc.tcp://opcplc-000000:50000 -g $RESOURCE_GROUP --cluster $CLUSTER_NAME
+az iot ops asset endpoint create --name $CONNECTOR_NAME --target-address $CONNECTOR_URL -g $RESOURCE_GROUP --cluster $CLUSTER_NAME
 
+echo ""
 kubectl get assetendpointprofile -n azure-iot-operations
 
+echo ""
 echo "Configure the simulator /!\ DO NOT USE IN PRODUCTION /!\ "
-kubectl patch AssetEndpointProfile opc-ua-connector-0 -n azure-iot-operations --type=merge -p '{"spec":{"additionalConfiguration":"{\"applicationName\":\"opc-ua-connector-0\",\"security\":{\"autoAcceptUntrustedServerCertificates\":true}}"}}'
+kubectl patch AssetEndpointProfile $CONNECTOR_NAME -n azure-iot-operations --type=merge -p '{"spec":{"additionalConfiguration":"{\"applicationName\":\"$CONNECTOR_NAME\",\"security\":{\"autoAcceptUntrustedServerCertificates\":true}}"}}'
 
-echo "Add an asset, tags, and events"
-az iot ops asset create --name thermostat -g $RESOURCE_GROUP --cluster $CLUSTER_NAME --endpoint opc-ua-connector-0 --description 'A simulated thermostat asset' --data  data_source='ns=3;s=FastUInt10', name=temperature --data data_source='ns=3;s=FastUInt100', name='Tag 10'
+echo "Adding assets with tags and events"
+#az iot ops asset create --name thermostat -g $RESOURCE_GROUP --cluster $CLUSTER_NAME --endpoint $CONNECTOR_NAME --description 'A simulated thermostat asset' --data  data_source='ns=3;s=FastUInt10', name=temperature --data data_source='ns=3;s=FastUInt100', name='Tag 10'
 
+# Loop to create multiple assets
+for i in $(seq 1 $NUM_THERMOSTATS); do
+    ASSET_NAME="thermostat$i"
+    echo "Adding asset: $ASSET_NAME"
+    az iot ops asset create --name $ASSET_NAME -g $RESOURCE_GROUP --cluster $CLUSTER_NAME --endpoint $CONNECTOR_NAME --description "A simulated thermostat asset $i" --data data_source='ns=3;s=FastUInt10', name=temperature --data data_source='ns=3;s=FastUInt100', name='Tag 10'
+done
+
+echo ""
 kubectl get pods -n azure-iot-operations
+
+# Verifying the data is flowing
+kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/explore-iot-operations/main/samples/quickstarts/mqtt-client.yaml
+
+kubectl exec --stdin --tty mqtt-client -n azure-iot-operations -- sh
+
+mosquitto_sub --host aio-mq-dmqtt-frontend --port 8883 --topic "azure-iot-operations/data/#" -v --debug --cafile /var/run/certs/ca.crt -D CONNECT authentication-method 'K8S-SAT' -D CONNECT authentication-data $(cat /var/run/secrets/tokens/mq-sat)
